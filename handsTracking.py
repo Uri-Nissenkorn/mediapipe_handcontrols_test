86815e7cfa2c0a5864bf3297464c232682ec9f40
Uri Nissenkorn <Uri.Nissenkorn@gmail.com>
2022-09-28 17:32:40 +0300

    confidence

--- handsTracking.py
+++ handsTracking.py
@@ -9,20 +9,24 @@ mp_draw = mp.solutions.drawing_utils
 
 hands = mp_hands.Hands()
 
-while True:
-    success, image = cap.read()
-    image = cv2.flip(image,1)
+with mp_hands.Hands(max_num_hands=1,min_detection_confidence=0.8,min_tracking_confidence=0.8):
+    while cap.isOpened():
+        success, image = cap.read()
+        image = cv2.flip(image,1)
 
-    img_RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(img_RGB)
-    
-    if results.multi_hand_landmarks:
-        for hand_landmark in results.multi_hand_landmarks:
-            mp_draw.draw_landmarks(image, hand_landmark, mp_hands.HAND_CONNECTIONS)
+        img_RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(img_RGB)
 
-    cv2.imshow("Hand Tracking", image)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+        if results.multi_hand_landmarks:
+            for hand_landmark in results.multi_hand_landmarks:
+                mp_draw.draw_landmarks(image, hand_landmark, mp_hands.HAND_CONNECTIONS)
+                
+
+
+
+        cv2.imshow("Hand Tracking", image)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
     
 cap.realease()
 cv2.destroyAllWindows()
